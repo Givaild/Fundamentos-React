e8a64eaeea4895db8ea6f2e8d0ea19a0851d2525
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-09-05 15:34:31 -0300

    fix:Add components

---
+++ src/componets/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Card({name,time}) {
+    return (
+        <div className='Card'>
+            <strong>{name}</strong>
+            <small>{time}</small>
+        </div>
+    )
+}
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,11 +1,24 @@
 import './styles.css';
+import { Card } from '../../componets/Card'; 
+import { useState } from 'react';
 
 export function Home() {
+  const [personName,setPersonName] = useState();
+  
   return (
-    <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome" />
+    <div className='container'>
+      <h1>Nome:{personName}</h1>
+      <input
+       type='text' 
+       placeholder='Digite o nome'
+       onChange={event => setPersonName(event.target.value)} 
+       />
       <button type="button">Adicionar</button>
+
+      <Card name='Givaildo' time='12:15:22'/>
+      <Card name='Gustavo' time="12:17:02"/>
+      <Card name='Henrique' time="12:20:00"/>
+      
     </div>
   )
 }
