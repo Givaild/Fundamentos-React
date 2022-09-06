3ea89f05435b93bbfd41d578febadced78d79c6f
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-09-06 15:26:59 -0300

    feat:isert use state and use effect

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,24 +1,62 @@
 import './styles.css';
 import { Card } from '../../componets/Card'; 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function Home() {
-  const [personName,setPersonName] = useState();
+  const [personName,setPersonName] = useState('');
+  const [persons,setPersons] = useState([]);
+  const [user,setUser] = useState({
+    name:'', avatar:''
+  });
+
+ function handleAddPerson(){
+    const newPerson = {
+        name:personName,
+        time: new Date().toLocaleTimeString('pt-br', {
+          hour:'2-digit',
+          minute:'2-digit',
+          second:'2-digit',
+      })
+    };
+    setPersons(prevState=>[...prevState,newPerson]);
+  }  
+
+  useEffect(()=> {
+    fetch ('https://api.github.com/users/givaild')
+    .then(response => response.json())
+    .then(data => {
+      setUser({
+        name:data.name,
+        avatar:data.avatar_url,
+      })
+    })
+  },[]);
   
   return (
     <div className='container'>
-      <h1>Nome:{personName}</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+      </header>
       <input
        type='text' 
        placeholder='Digite o nome'
        onChange={event => setPersonName(event.target.value)} 
        />
-      <button type="button">Adicionar</button>
+      <button
+       type="button"
+       onClick={handleAddPerson}>
+        Adicionar
+      </button>
 
-      <Card name='Givaildo' time='12:15:22'/>
-      <Card name='Gustavo' time="12:17:02"/>
-      <Card name='Henrique' time="12:20:00"/>
-      
+     {
+      persons.map(thePerson => <Card name={thePerson.name} time={thePerson.time}/>)
+     
+     }
+           
     </div>
   )
 }
